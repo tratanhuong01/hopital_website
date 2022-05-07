320f4a35fdc6af8d320edcea153a6ae83c4c9463
tratanhuong01 <tratanhuong01@gmail.com>
2022-05-07 10:41:03 +0700

    update

--- js/app.js
+++ js/app.js
@@ -9,18 +9,27 @@ const bannerSearchInput = document.querySelector('.banner__search--input input')
 const bannerSearchPopup = document.querySelector('.banner__search--popup');
 //
 
+//
+const postEndHandle = document.querySelectorAll('.post__end--handle');
+const postEndDoc = document.querySelector('.post__end--doc');
+const postEndFeedback = document.querySelector('.post__end--feedback');
+//
 
 //
+const viewMore = document.querySelector('.view__more');
+const orderContainer = document.querySelector('.order__container');
 //
 
-bannerSearchInput.addEventListener('click', function () {
-    if (bannerSearchPopup.classList.contains('active')) {
-        bannerSearchPopup.classList.remove('active');
-    }
-    else {
-        bannerSearchPopup.classList.add('active');
-    }
-});
+if (bannerSearchInput && bannerSearchPopup) {
+    bannerSearchInput.addEventListener('click', function () {
+        if (bannerSearchPopup.classList.contains('active')) {
+            bannerSearchPopup.classList.remove('active');
+        }
+        else {
+            bannerSearchPopup.classList.add('active');
+        }
+    });
+}
 
 headerIcon.addEventListener('click', () => {
     const clickOutSide = () => {
@@ -77,4 +86,30 @@ $('.item__guide').slick({
     ]
 })
 
+if (postEndHandle && postEndDoc && postEndFeedback) {
+    const postEndList = [postEndDoc, postEndFeedback];
+    [...postEndHandle].forEach((el, index) => {
+        el.addEventListener('click', () => {
+            if (postEndList[index].classList.contains('active')) {
+                postEndList[index].classList.remove('active')
+            }
+            else {
+                postEndList[index].classList.add('active')
+            }
+        })
+    })
+}
 //
+
+if (viewMore && orderContainer) {
+    viewMore.addEventListener('click', function () {
+        if (!orderContainer.classList.contains('show')) {
+            viewMore.innerHTML = 'Ẩn bớt';
+            orderContainer.classList.add('show');
+        }
+        else {
+            viewMore.innerHTML = 'Xem thêm';
+            orderContainer.classList.remove('show');
+        }
+    });
+}
